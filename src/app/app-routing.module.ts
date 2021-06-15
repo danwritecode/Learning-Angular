b08d81fe563e741d9856746366bd43ba1962d80a
nelsynelz <dnelson4993@outlook.com>
2021-06-14 20:56:44 -0400

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ArticleListComponent } from './article-list/article-list.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'article-list', component: ArticleListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
